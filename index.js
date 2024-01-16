86a6ef8fa55345d06c27df2bc618513f2c1a5fbc
Gustavo Blasius <gustavo.blasius@clinicorp.com>
2024-01-15 23:06:30 -0300

    feat: try execute tasks with setTimeout
    obs* it's not working

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
 
-function Queue(){
+function Queue() {
   this.elements = [];
 
 }
 // Adiciona um elemento no final da fila
-Queue.prototype.enqueue = function(e) {
+Queue.prototype.enqueue = function (e) {
   this.elements.push(e)
 }
 
 // Remove o primeiro elemento da fila
-Queue.prototype.dequeue = function() {
+Queue.prototype.dequeue = function () {
   return this.elements.shift();
 }
 
 // checa se a fila está vazia
-Queue.prototype.isEmpty = function() {
+Queue.prototype.isEmpty = function () {
   return this.elements.length == 0;
 }
 
-Queue.prototype.peek = function() {
-  return !this.isEmpty() ? this.elements[0] : undefined;
+Queue.prototype.peek = function () {
+  return !this.isEmpty() ? this.elements : undefined;
 }
 
-Queue.prototype.executeTask = function(){
-  while(!this.isEmpty()){
-    console.log(`Executando task: ${11 - this.elements.length}. o Valor da task é ${this.elements}`)
-    this.dequeue();
+Queue.prototype.executeTask = async function () {
+  while (!this.isEmpty()) {
+    const promise = this.elements[0]
+    const result = await promise;
+    console.log('result', result)
+    this.dequeue()
   }
 
   console.log('tasks finalizadas')
 }
 
+function testPromise(){
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=> {
+      resolve('teste')
+    }, 2000)
+  })  
+}
+
 let task = new Queue()
 
 for (let i = 0; i < 10; i++) {
-  task.enqueue(i)
+  task.enqueue(testPromise())
 }
+
+// console.log(task.peek())
 
 task.executeTask()
